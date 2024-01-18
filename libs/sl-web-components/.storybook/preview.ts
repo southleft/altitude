4bae09d149c769cdbe16af9e0aa865559b75640b
Elsa Brown <elsambrown@gmail.com>
2024-01-18 14:57:05 -0500

    Registers custom components

--- libs/sl-web-components/.storybook/preview.ts
+++ libs/sl-web-components/.storybook/preview.ts
@@ -1,5 +1,8 @@
 import type { Preview } from '@storybook/web-components';
 
+// Setting auto registry to true to allow storybook to automatically register components
+globalThis.enAutoRegistry = true;
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
