cec3200902ada178d4e69cba2f63bf42e08fa8a7
Kelsey Cahill <kelscahill@gmail.com>
2024-01-31 10:39:29 -0500

    Storybook updates

--- libs/sl-web-components/.storybook/preview.ts
+++ libs/sl-web-components/.storybook/preview.ts
@@ -1,20 +1,44 @@
 import type { Preview } from '@storybook/web-components';
+
+/*
+ * Custom Elements
+ * - Set custom elements manifest before any code runs. This enables autodocs from web components jsdoc comments.
+ */
 import { setCustomElementsManifest } from '@storybook/web-components';
 import customElements from '../custom-elements.json';
-
-// Set custom elements manifest before any code runs. This enables autodocs from web components jsdoc comments.
 setCustomElementsManifest(customElements);
 
-// Setting auto registry to true to allow storybook to automatically register components
+/*
+ * MFE's
+ * - Setting auto registry to true to allow storybook to automatically register components
+ */
 globalThis.enAutoRegistry = true;
 
-// Creating a style element for headStyles and appending it to the document head
+/*
+ * Storybook styles
+ * - Allows for custom styles of the storybook UI
+ * - Creating a style element for storybookStyles and appending it to the window parent document head
+ */
+import storybookStyles from '../styles/storybook.scss';
+const storybookStyleElement = document.createElement('style');
+storybookStyleElement.innerHTML = storybookStyles;
+window.parent.document.head.appendChild(storybookStyleElement);
+
+/*
+ * Head styles
+ * - Allows for custom styles of the story iframe window
+ * - Creating a style element for headStyles and appending it to the document head
+ */
 import headStyles from '../styles/head.scss';
 const headStyleElement = document.createElement('style');
 headStyleElement.innerHTML = headStyles;
 document.head.appendChild(headStyleElement);
 
-// Creating a style element for iconFontCSS and appending it to the document head
+/*
+ * Icon font styles
+ * - Allows for the icon font to be avaiable in the story iframe window
+ * - Creating a style element for iconFontCSS and appending it to the document head
+ */
 import iconFontCSS from '../components/icon/fonts/iconfont.css';
 const iconFontStyleElement = document.createElement('style');
 iconFontStyleElement.setAttribute('type', 'text/css');
@@ -22,6 +46,10 @@ iconFontStyleElement.setAttribute('id', 'iconfont-style');
 iconFontStyleElement.innerHTML = iconFontCSS;
 document.head.appendChild(iconFontStyleElement);
 
+/*
+ * Exclude's
+ * - Properties and methods to be excluded from the storybook controls
+ */
 export const excludeRegexArray = [
   '^children$',
   '^render$',
@@ -62,7 +90,8 @@ const preview: Preview = {
       storySort: {
         order: ['Documentation', 'Fundamentals', 'Atoms', 'Molecules', 'Organisms', 'Templates', 'Recipes']
       }
-    }
+    },
+    backgrounds: { disable: true },
   }
 };
 
