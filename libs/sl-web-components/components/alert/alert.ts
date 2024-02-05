2a3a4b564deea73b991253cb5daaa7eed314fb96
Elsa Brown <elsambrown@gmail.com>
2024-02-05 11:21:39 -0500

    Updates to Alert

--- libs/sl-web-components/components/alert/alert.ts
+++ libs/sl-web-components/components/alert/alert.ts
@@ -1,16 +1,15 @@
 import { TemplateResult, unsafeCSS } from 'lit';
 import { property } from 'lit/decorators.js';
-import { ifDefined } from 'lit/directives/if-defined.js';
 import { html, unsafeStatic } from 'lit/static-html.js';
-import { nanoid } from 'nanoid';
 import register from '../../directives/register';
 import PackageJson from '../../package.json';
 import { SLElement } from '../SLElement';
-import { SLIconChevronDown } from '../icon/icons/chevron-down';
 import { SLIconDone } from '../icon/icons/done';
 import { SLIconInfo } from '../icon/icons/info';
 import { SLIconWarningCircle } from '../icon/icons/warning-circle';
 import { SLIconWarningTriangle } from '../icon/icons/warning-triangle';
+import { SLIconClose } from '../icon/icons/close';
+import { SLButton } from '../button/button';
 import styles from './alert.scss';
 
 /**
@@ -18,7 +17,6 @@ import styles from './alert.scss';
  *
  * Alert displays a short important message that will attract a user's attention without interrupting the user's task.
  * - **slot**: The alert's main content or title
- * - **slot** "description": A description to include in the dropdown panel
  * - **slot** "icon": Slot in an icon to override the default one
  * */
 export class SLAlert extends SLElement {
@@ -26,20 +24,22 @@ export class SLAlert extends SLElement {
 
   private elementMap = register({
     elements: [
-      [SLIconChevronDown.el, SLIconChevronDown],
       [SLIconDone.el, SLIconDone],
       [SLIconInfo.el, SLIconInfo],
       [SLIconWarningCircle.el, SLIconWarningCircle],
-      [SLIconWarningTriangle.el, SLIconWarningTriangle]
+      [SLIconWarningTriangle.el, SLIconWarningTriangle],
+      [SLIconClose.el, SLIconClose],
+      [SLButton.el, SLButton]
     ],
     suffix: (globalThis as any).enAutoRegistry === true ? '' : PackageJson.version
   });
 
-  private iconChevronDownEl = unsafeStatic(this.elementMap.get(SLIconChevronDown.el));
   private iconDoneEl = unsafeStatic(this.elementMap.get(SLIconDone.el));
   private iconInfoEl = unsafeStatic(this.elementMap.get(SLIconInfo.el));
   private iconWarningEl = unsafeStatic(this.elementMap.get(SLIconWarningCircle.el));
   private iconWarningTriangleEl = unsafeStatic(this.elementMap.get(SLIconWarningTriangle.el));
+  private iconCloseEl = unsafeStatic(this.elementMap.get(SLIconClose.el));
+  private buttonEl = unsafeStatic(this.elementMap.get(SLButton.el));
 
   static get styles() {
     return unsafeCSS(styles.toString());
@@ -56,6 +56,12 @@ export class SLAlert extends SLElement {
   accessor variant: 'success' | 'warning' | 'danger';
 
   /**
+   * Optional title of the alert
+   */
+  @property()
+  accessor title: string;
+
+  /**
    * isActive boolean
    * - If true, the alert displays on the page
    * - If false, the alert is hidden from the page
@@ -64,30 +70,14 @@ export class SLAlert extends SLElement {
   accessor isActive: boolean;
 
   /**
-   * isExpanded boolean
-   * - If true, the panel will be expanded
-   * - If false, the panel will be collapsed
-   */
-  @property({ type: Boolean })
-  accessor isExpanded: boolean = false;
-
-  /**
-   * hasPanel boolean
-   * 1. If true, include the dropdown panel and expand icon
-   * 2. Default value is false unless description is provided
-   */
-  @property({ type: Boolean })
-  accessor hasPanel: boolean = false;
-
-  /**
    * Auto close?
    * - Set whether you want the alert to auto close. Adjust the autoCloseDelay if you want longer than 3 seconds
    */
   @property({ type: Boolean })
-  accessor autoClose: boolean;
+  accessor autoClose: boolean = false;
 
   /**
-   * Delay property
+   * Auto close delay
    * 1. Number of seconds to close the alert when autoClose is enabled
    * 2. Default amount is 3
    */
@@ -95,18 +85,11 @@ export class SLAlert extends SLElement {
   accessor autoCloseDelay: number = 3; /* 2 */
 
   /**
-   * Aria Controls attribute
-   * - Dynamically set for A11y
+   * Dismissible?
+   * - If true, displays a close button with close functionality on the alert
    */
-  @property()
-  accessor ariaControls: string;
-
-  /**
-   * Aria Labelled By attribute
-   * - Dynamically set for A11y
-   */
-  @property()
-  accessor ariaLabelledBy: string;
+  @property({ type: Boolean })
+  accessor isDismissible: boolean;
 
   /**
    * Internal property store setTimeout() method so that we can clear timer later
@@ -114,60 +97,11 @@ export class SLAlert extends SLElement {
   private _timer: ReturnType<typeof setTimeout>;
 
   /**
-   * Connected callback
-   */
-  connectedCallback() {
-    super.connectedCallback();
-  }
-
-  /**
    * When the component is first updated
-   * 1. Sets hasPanel to true if panel description is provided
-   * 2. Set aria attributes for A11y
-   * 3. Initializes auto close if there is no panel
+   * 1. Initializes auto close if there is no panel
    */
   firstUpdated() {
-    this.hasPanel = this.slotNotEmpty('panel'); /* 1 */
-    this.setAria(); /* 2 */
-
-    /* 3 */
-    this.handleAutoClose();
-  }
-
-  /**
-   * Add aria attributes for A11y
-   * 1. Dynamically sets the aria-labelledby for A11y
-   * 2. If there is a panel, then add the aria-controls
-   */
-  setAria() {
-    this.ariaLabelledBy = this.ariaLabelledBy || nanoid();
-    /* 2 */
-    if (this.hasPanel) {
-      this.ariaControls = this.ariaControls || nanoid();
-    }
-  }
-
-  /**
-   * Handle on keydown
-   * 1. If enter key is pressed, expand the panel
-   * 2. If the panel is expanded and escape is keyed, close the panel
-   * 3. Return focus to the trigger button
-   */
-  handleOnKeydown(e: KeyboardEvent) {
-    if (this.hasPanel) {
-      /* 1 */
-      if (this.isExpanded === false && e.code === 'Enter') {
-        this.toggleExpanded();
-      }
-
-      /* 2 */
-      if (this.isExpanded === true && e.code === 'Escape') {
-        this.toggleExpanded();
-        /* 3 */
-        const trigger = this.shadowRoot.querySelector<HTMLElement>('.sl-c-alert__header');
-        trigger.focus();
-      }
-    }
+    this.handleAutoClose(); /* 1 */
   }
 
   /**
@@ -202,33 +136,6 @@ export class SLAlert extends SLElement {
   }
 
   /**
-   * Toggle expanded
-   * 1. Toggle the expanded state
-   * 2. Dispatch the custom change event
-   */
-  public toggleExpanded() {
-    /* 1 */
-    this.isExpanded = !this.isExpanded;
-
-    /* 2 */
-    if (this.isExpanded) {
-      this.dispatch({
-        eventName: 'onAlertExpand',
-        detailObj: {
-          expanded: this.isExpanded
-        }
-      });
-    } else {
-      this.dispatch({
-        eventName: 'onAlertCollapse',
-        detailObj: {
-          expanded: this.isExpanded
-        }
-      });
-    }
-  }
-
-  /**
    * Close alert
    * 1. Set isActive to false to hide the alert
    * 2. Dispatch a custom event on close
@@ -236,10 +143,11 @@ export class SLAlert extends SLElement {
   public close() {
     /* 1 */
     this.isActive = false;
+    console.log("active? ", this.isActive)
 
     /* 2 */
     this.dispatch({
-      eventName: 'onAlertClose',
+      eventName: 'close',
       detailObj: {
         active: this.isActive
       }
@@ -257,7 +165,7 @@ export class SLAlert extends SLElement {
 
     /* 2 */
     this.dispatch({
-      eventName: 'onAlertOpen',
+      eventName: 'open',
       detailObj: {
         active: this.isActive
       }
@@ -269,9 +177,8 @@ export class SLAlert extends SLElement {
       'sl-c-alert--success': this.variant === 'success',
       'sl-c-alert--warning': this.variant === 'warning',
       'sl-c-alert--danger': this.variant === 'danger',
-      'sl-is-active': this.isActive === true,
-      'sl-is-expanded': this.isExpanded === true,
-      'sl-c-alert--has-panel': this.hasPanel === true
+      'sl-is-active': this.isActive,
+      'sl-has-title': !!this.title
     });
 
     let alertIcon = html`<${this.iconInfoEl}></${this.iconInfoEl}>`;
@@ -288,19 +195,24 @@ export class SLAlert extends SLElement {
       <div
         role="alert"
         class=${componentClassNames}
-        @keydown=${this.handleOnKeydown}
-        @mouseover=${this.handleMouseOver}
-        @mouseleave=${this.handleMouseLeave}
-        aria-labelledby=${this.ariaLabelledBy}
-        aria-expanded=${ifDefined(this.isExpanded)}
       >
         <div class="sl-c-alert__icon">${this.slotNotEmpty('icon') ? 
           html` <slot name="icon"></slot> ` : html` ${alertIcon} `}
         </div>
         <div class="sl-c-alert__body">
-          ${this.title ? 
-            html`<div class="sl-c-alert__title">${this.title}</div>` : ''}
-          <slot></slot>
+          <div class="sl-c-alert__content">
+            ${this.title ? 
+              html`<div class="sl-c-alert__title">${this.title}</div>` : ''}
+            <slot></slot>
+          </div>
+          <div class="sl-c-alert__actions">
+            ${this.slotNotEmpty('action') ?
+              html`<div class="sl-c-alert__action">
+                <slot name="action"></slot>
+              </div>` : ''}
+            ${this.isDismissible ? 
+              html`<${this.buttonEl} class="sl-c-alert__close" variant="tertiary" hideText="true" label="Close" @click=${this.close}><${this.iconCloseEl} slot="before"></${this.iconCloseEl}></${this.buttonEl}>` : ''}
+            </div>
         </div>
       </div>
     ` as TemplateResult<1>;
