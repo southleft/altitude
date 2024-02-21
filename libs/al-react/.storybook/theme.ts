5ad82b95f71a83fecad3382eecdc58cbd209763b
Kelsey Cahill <kelscahill@gmail.com>
2024-02-21 16:52:29 -0500

    Build update

--- libs/al-react/.storybook/theme.ts
+++ libs/al-react/.storybook/theme.ts
@@ -3,7 +3,7 @@ import { create } from '@storybook/theming/create';
 export default create({
   base: 'dark',
   brandTitle: 'Altitude React Design System',
-  brandUrl: 'https://altitude-storybook-react.pages.dev/',
+  brandUrl: 'https://altitude.pages.dev/',
   brandImage: './images/logo.svg',
   fontBase: '"IBM Plex Sans", sans-serif',
 });
