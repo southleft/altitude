5b24a8a7536f2797a52f17effa629feef3307e36
Kelsey Cahill <kelscahill@gmail.com>
2024-02-21 16:59:27 -0500

    Build update

--- libs/al-react/.storybook/theme.ts
+++ libs/al-react/.storybook/theme.ts
@@ -3,7 +3,7 @@ import { create } from '@storybook/theming/create';
 export default create({
   base: 'dark',
   brandTitle: 'Altitude React Design System',
-  brandUrl: 'https://altitude.pages.dev/',
+  brandUrl: 'https://altitude.pages.dev/libs/al-react/storybook-static',
   brandImage: './images/logo.svg',
   fontBase: '"IBM Plex Sans", sans-serif',
 });
--- libs/al-web-components/.storybook/theme.js
+++ libs/al-web-components/.storybook/theme.js
@@ -3,7 +3,7 @@ import { create } from '@storybook/theming/create';
 export default create({
   base: 'dark',
   brandTitle: 'Altitude Web Components Design System',
-  brandUrl: 'https://altitude.pages.dev/',
+  brandUrl: 'https://altitude.pages.dev/libs/al-web-components/storybook-static',
   brandImage: './images/logo.svg',
   fontBase: '"IBM Plex Sans", sans-serif',
 });
