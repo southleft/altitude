8fffec268218d453262190cc9c72e9e3fd48dbc5
Elsa Brown <elsambrown@gmail.com>
2024-02-27 10:30:50 -0500

    Update webpack public path for production

--- apps/react/webpack.config.js
+++ apps/react/webpack.config.js
@@ -1,33 +1,36 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-module.exports = {
-  output: {
-    path: path.resolve(__dirname, 'demo'),
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      {
-        test: [ /\.js$/, /\.jsx$/, /\.ts$/, /\.tsx$/ ],
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+module.exports = (env, argv) => {
+  return {
+    output: {
+      path: path.resolve(__dirname, 'demo'),
+      filename: "main.js",
+      publicPath: argv.mode ==="production" ? "./" : "/"
+    },
+    module: {
+      rules: [
+        {
+          test: [ /\.js$/, /\.jsx$/, /\.ts$/, /\.tsx$/ ],
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
         },
-      },
-      { test: [/\.css$/, /\.scss$/],
-        exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'sass-loader'] 
-      },
-    ],
-  },
-  devServer: {
-    historyApiFallback: true
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-      filename: "./index.html",
-    }),
-  ]
+        { test: [/\.css$/, /\.scss$/],
+          exclude: /node_modules/,
+          use: ['style-loader', 'css-loader', 'sass-loader'] 
+        },
+      ],
+    },
+    devServer: {
+      historyApiFallback: true
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+        filename: "./index.html",
+      }),
+    ]
+  };
 };
