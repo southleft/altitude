25a1533632b2266ce89872df307d0d5ff5e5807c
bgreater <bvoran@gmail.com>
2024-02-27 14:27:02 -0500

    adding button to angular

--- apps/angular/src/app/app.component.ts
+++ apps/angular/src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import "al-web-components/dist/css/head.css";
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
   title = 'angular';
+
+  constructor() {
+    if (globalThis.customElements) {
+      // Load the web component when the app is loaded
+      import("al-web-components/dist/components/button/button.js");
+    }
+  }
+
 }
