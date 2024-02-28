cb8439babc10264fa8d75d460649a4d2be0ca2f7
Elsa Brown <elsambrown@gmail.com>
2024-02-28 12:27:55 -0500

    Built angular demo

---
+++ apps/angular/src/app/f-po/f-po.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fpo',
+  templateUrl: './f-po.component.html',
+  styleUrl: './f-po.component.scss'
+})
+export class FpoComponent {}
+
